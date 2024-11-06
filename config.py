9c26893836b957ef1c95c13b11221d0d532ef39b
jhjcpishva <jhjcpishva@users.noreply.github.com>
2024-11-06 13:39:36 +0900

    support ENV

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+
+load_dotenv(verbose=True)
+
+dotenv_path = join(dirname(__file__), os.getenv("DOTENV", '.env'))
+load_dotenv(dotenv_path)
+
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", "")
+LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET", "")
+LLMS_HOST = os.getenv("LLMS_HOST", "")
+CONFIG_FILE = os.getenv("CONFIG_FILE", "_config.json")
+
+PORT = int(os.getenv("PORT", "8001"))
--- main.py
+++ main.py
@@ -11,6 +11,8 @@ from linebot.v3.messaging import (
     TextMessage,
 )
 
+import config
+
 
 @dataclass
 class AppConfig:
@@ -19,35 +21,56 @@ class AppConfig:
     user_id: str
 
 
-LLMS_HOST = "http://localhost:8000"
 app: Flask
 
 
 def setup_app():
-    access_token = input("input LINE channel access token: ")
-    assert (len(access_token) != 0)
-    secret = input("input LINE channel secret: ")
-    assert (len(secret) != 0)
-    print(f"open {LLMS_HOST}/login and input the code")
+    """
+    setup config.json
+    """
+    access_token: str
+    secret: str
+
+    if len(config.LINE_CHANNEL_ACCESS_TOKEN):
+        access_token = config.LINE_CHANNEL_ACCESS_TOKEN
+    else:
+        access_token = input("input LINE channel access token: ")
+        assert (len(access_token) != 0)
+
+    if len(config.LINE_CHANNEL_SECRET):
+        secret = config.LINE_CHANNEL_SECRET
+    else:
+        secret = input("input LINE channel access token: ")
+        assert (len(secret) != 0)
+
+    # open LineLoginMicroService
+    login_url = f"{config.LLMS_HOST}/login"
+    print(f"open '{login_url}' and input the code")
+    import webbrowser
+    webbrowser.open(login_url, autoraise=True)
+
     code = input("code: ")
     assert (len(code) != 0)
 
-    auth_collect = requests.post(f"{LLMS_HOST}/api/v1/auth/collect", json={"code": code})
+    auth_collect = requests.post(f"{config.LLMS_HOST}/api/v1/auth/collect", json={"code": code})
     session_id = auth_collect.json()["session"]
-    profile = requests.get(f"{LLMS_HOST}/api/v1/sessions/{session_id}/")
+    profile = requests.get(f"{config.LLMS_HOST}/api/v1/sessions/{session_id}/")
     print(profile.json())
 
-    with open('config.json', 'w') as fp:
+    with open(config.CONFIG_FILE, 'w') as fp:
         json.dump({
             "access_token": access_token,
             "secret": secret,
             "user_id": profile.json()["user_id"],
-        }, fp=fp)
+        }, fp=fp, indent=2)
 
-    print("done")
+    print(f"Configuration saved to '{config.CONFIG_FILE}'")
 
 
 def serve_app(config: AppConfig):
+    """
+    Flask WebServer
+    """
     app = Flask(__name__)
     app.config["DEBUG"] = True
 
@@ -74,12 +97,12 @@ def serve_app(config: AppConfig):
 
 
 def load_config() -> AppConfig:
-    with open("config.json", "r") as fp:
-        config = json.load(fp)
+    with open(config.CONFIG_FILE, "r") as fp:
+        data = json.load(fp)
         return AppConfig(
-            line_channel_access_token=config["access_token"],
-            line_channel_secret=config["secret"],
-            user_id=config["user_id"]
+            line_channel_access_token=data["access_token"],
+            line_channel_secret=data["secret"],
+            user_id=data["user_id"]
         )
 
 
@@ -90,6 +113,6 @@ if __name__ == "__main__":
         app_config = load_config()
     except Exception as e:
         setup_app()
-        exit(0)
+        app_config = load_config()
 
     serve_app(app_config)
